409455e4b6b5e9863827d817ce38a47b021f33a7
Ishan Shagireev <ishan.shagireev@student.htldornbirn.at>
2025-01-10 09:28:26 +0100

    Button aus dem Video gemacht

---
+++ firstapp/src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import style from "./Button.module.css";
+
+export default function Button(props) {
+    const [isOn, setIsOn] = useState()
+
+    return (
+        <div 
+         className={isOn?style.isOn:style.isOff}
+         onClick={() => {
+            alert("clicked");
+            setIsOn(!isOn);
+         }}
+        > 
+        
+        {props.name} is {props.age} years old and is {isOn?"On":"Off"}
+        </div>
+    );
+}
